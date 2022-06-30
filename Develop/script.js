3dde116084050257123b9836c797a4301fcc0de2
CarlosE3595 <cmejr95@gmail.com>
2022-06-30 02:45:42 -0700

    implemented loops for password generator function

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,22 @@
-const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "123456789";
-const symbols = "!@#$%^&*()-=_+[]{}|;:',./<>?";
-
-
-
-
 // Assignment code here
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+
+  var rand = Math.random()
+  return Math.floor(min* (1 - rand) + rand*max)
+}
+
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+
+}
+
+
+
+
 function generatePassword() {
 
   var userInput = prompt("How long would you like your password to be?")
@@ -14,7 +24,7 @@ function generatePassword() {
   var passwordLength = parseInt(userInput)
 
   if (isNaN(passwordLength)) {
-    window.alert("Input you placed is not a number.")
+    window.alert("Input you placed is not valid.")
     return
   }
 
@@ -23,32 +33,53 @@ function generatePassword() {
     return
   }
 
+  var userWantsLowercaseLetters = window.confirm("Would you like to include lowercase letters into your password?");
+  var userWantsUppercaseLetters = window.confirm("Would you like to include uppercase letters into your password?");
+  var userWantsNumbers = window.confirm("Would you like to include numbers into your password?");
+  var userWantsSymbols = window.confirm("Would you like to include symbols into your password?");
 
-//   var characters = ""
-//   var userInput = prompt("How would you like to generate your password?\n1-Uppercase letters\n2-Lowercase letter\n3-numbers\n4-symbols")
-//   if (userInput == 1) {
-//     characters += lowercaseLetters
-//   } 
-//   else if(userInput == 2) {
-//    characters += lowercaseLetters += uppercaseLetters
+  var numberList = ["1","2","3","4","5","6","7","8","9"]
+  var symbolList = ["!","@","#","$","%","^","&","*","(",")","-","=","_","+","[","]","{","}","|",";",":","'",",",".","/","<",">","?"]
+  var lowercaseLettersList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var uppercaseLettersList = []
+  var optionscart = []
 
-//   } 
-//   else if(userInput == 3){
-//     characters += lowercaseLetters += uppercaseLetters += numbers
-//   } 
-//   else if(userInput == 4){
-//     characters += lowercaseLetters += uppercaseLetters += numbers += symbols
-   
-//   }
-//   randomPassword(characters, length)
-//  }
+  for (var i = 0; i < lowercaseLettersList.length; i++) {
+    uppercaseLettersList[i] = lowercaseLettersList[i].toUpperCase()
+  }
 
-// function randomPassword(characters, length){
-//   while(s) {
+  if (userWantsNumbers === true) {
+    optionscart.push(numberList)
+  }
+  if (userWantsSymbols === true) {
+    optionscart.push(symbolList)
+  }
+  if (userWantsLowercaseLetters === true) {
+    optionscart.push(lowercaseLettersList)
+  }
+  if (userWantsUppercaseLetters === true) {
+    optionscart.push(uppercaseLettersList)
+  }
+  if (optionscart.length === 0) {
+    optionscart.push(lowercaseLettersList)
+  }
+
+  var generatedPassword = ""
+  for (var i = 0; i < passwordLength; i++) { 
+    var randomList = getRandomItem(optionscart)
+    var randomChar = getRandomItem(randomList)
+    generatedPassword += randomChar
     
-//   }
-
+  }
+  
+  return generatedPassword
  }
+
+
+
+
+
+
 
 
 
